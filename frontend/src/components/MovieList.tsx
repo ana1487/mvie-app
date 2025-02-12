644a128e8cfc99cb9a12facc4c00cf7c5e8ec8a4
ana1487 <ana1487@my.londonmet.ac.uk>
2025-02-12 22:47:56 +0545

    made simple changes to image and favourite button will need to make api call for movie detail when image is clicked

--- frontend/src/components/MovieList.tsx
+++ frontend/src/components/MovieList.tsx
@@ -7,14 +7,27 @@ interface MovieListProps {
 }
 
 const MovieList = (props: MovieListProps) => {
+    const [favourites, setFavourites] = React.useState<Movie[]>([]);
+
+    const addfavFunc = () => {
+        //This function will be used to add the movie to the favourite list
+        //We will need to implement this function in the next step
+        // setFavourites([...favourites, prop.movies.map((movie, index) => movie)]);
+    }
+
+    const openDetail = () => {
+        //This function will basically pull the information of the movie and display it in a modal
+        //will need to do api call to OMDB here.
+    }
+
     return (
         //added a div with a class name of movie-list-container to style the movie list
         <div className='movie-list-container'>
             {/*We could also get rid of the props but its okay for now.  */}
             {props.movies.map((movie, index) => (
                 <div key={index} className='movie-card' >
-                    <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                    <button>Add to favourites</button> {/*This add to favourite button will be needed to add the movie to the favourite list*/}
+                    <img onClick={openDetail} src={movie.Poster} alt={`${movie.Title} poster`} />
+                    <button className="btn-fav" onClick={addfavFunc}>Add to favourites</button> {/*This add to favourite button will be needed to add the movie to the favourite list*/}
                 </div>
             ))}
         </div>
