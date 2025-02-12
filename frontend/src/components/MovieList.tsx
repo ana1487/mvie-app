4b2137f3e0ad164294a7020dd1a75d6bda4f1f60
ana1487 <ana1487@my.londonmet.ac.uk>
2025-02-12 20:47:34 +0545

    Made appropriate changes to movielist  so that it is responsive and has movie cards in grid format

--- frontend/src/components/MovieList.tsx
+++ frontend/src/components/MovieList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Movie } from '../types/Movie.ts';
+import './MovieList.scss';
 
 interface MovieListProps {
     movies: Movie[];
@@ -7,13 +8,15 @@ interface MovieListProps {
 
 const MovieList = (props: MovieListProps) => {
     return (
-        <>
+        //added a div with a class name of movie-list-container to style the movie list
+        <div className='movie-list-container'>
+            {/*We could also get rid of the props but its okay for now.  */}
             {props.movies.map((movie, index) => (
-                <div key={index} style={{ border: '1px solid #ccc', margin: '10px', padding: '10px', borderRadius: '5px' }}>
-                    <img src={movie.Poster} alt={`${movie.Title} poster`} style={{ width: '200px', height: '300px' }} />
+                <div key={index} className='movie-card' >
+                    <img src={movie.Poster} alt={`${movie.Title} poster`} />
                 </div>
             ))}
-        </>
+        </div>
     );
 }
 
