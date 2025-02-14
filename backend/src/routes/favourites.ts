82320d702fd7315b1e379bc08e4b94037446aaac
ana1487 <ana1487@my.londonmet.ac.uk>
2025-02-14 20:35:15 +0545

    Add some comments and then deploy to Vercel

--- backend/src/routes/favourites.ts
+++ backend/src/routes/favourites.ts
@@ -3,11 +3,10 @@ import { getFavourites, getFavouriteById, addFavourite, deleteFavourite } from '
 
 const router = express.Router();
 
-//Define the routes
-router.get('/api/favourites', getFavourites); //getting all favourites from the database
-router.get('/api/favourites/:id', getFavouriteById); //gettting single favourtie item from the database
-router.post('/api/favourites', addFavourite); //this adds the favourite into thed database
-router.delete('/api/favourites/:id', deleteFavourite); //Helps to delete one favourite from the list based on the id
-
+// Define the routes
+router.get('/api/favourites', getFavourites); // getting all favourites from the database
+router.get('/api/favourites/:id', getFavouriteById); // getting single favourite item from the database
+router.post('/api/favourites', addFavourite); // this adds the favourite into the database
+router.delete('/api/favourites/:imdbID', deleteFavourite); // helps to delete one favourite from the list based on the imdbID
 
 export default router;
