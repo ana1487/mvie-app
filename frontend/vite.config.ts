889807416e66c11bf2712f92015bed6acb1fbafa
ana1487 <ana1487@my.londonmet.ac.uk>
2025-02-15 18:19:44 +0545

    making dist available for now

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    target: 'esnext',
+    outDir: 'dist'
   },
-  base: '/',
+  base: '/', // Ensure the base path is set correctly for Vercel deployment
 })
