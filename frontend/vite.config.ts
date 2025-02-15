af761f145322738a8d91122804d51ff6dc156b4a
ana1487 <ana1487@my.londonmet.ac.uk>
2025-02-15 17:31:09 +0545

    ... changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  base: '/', // Ensure the base path is set correctly for Vercel deployment
 })
